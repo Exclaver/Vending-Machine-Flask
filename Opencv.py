c63c02e16f8bd18b03ac8b2e884445d0671b1ece
Exclaver <devansh0403@gmail.com>
2024-02-13 14:25:06 +0530

    Fianl 2

--- Opencv.py
+++ Opencv.py
@@ -395,7 +395,3 @@ def index():
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
